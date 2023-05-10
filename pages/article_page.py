8236cffec991a8684a6d95f3db1bd568cdf973e2
LAPTOP-E367P135\User <vsmihey@gmail.com>
2023-05-10 23:19:33 +0700

    test_fixing_article

---
+++ pages/article_page.py
@@ -0,0 +1,194 @@
+import datetime
+import pathlib
+import random
+import time
+from pathlib import Path
+import selenium
+from selenium.common.exceptions import TimeoutException, ElementClickInterceptedException, NoSuchElementException
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.actions.action_builder import ActionBuilder
+from selenium.webdriver.common.by import By
+from generator.generator import generated_person
+from pages.base_page import BasePage
+from locators.form_pages_locators import FormPagesLocators as Locators
+# from locators.form_pages_locators import FixingArticle as Locators
+from pages.data_login_password import *
+from selenium.webdriver.common.alert import Alert
+
+
+class ArticlePage(BasePage):
+
+    def input_in_my_project(self, driver):
+        """INPUT IN MY PROJECT"""
+        self.element_is_visible(Locators.TYPE_AUTHOR).send_keys('Встроенный')
+        self.element_is_visible(Locators.LOGIN).send_keys(login)
+        self.element_is_visible(Locators.PASSWORD).send_keys(password)
+        self.element_is_visible(Locators.INPUT_BUTTON).click()
+        self.element_is_visible(Locators.TEST_PROJECT).click()
+
+    def add_normal_article(self, driver):
+        person = generated_person()
+        # last_name = person.last_name
+        first_name = person.first_name
+        text = "Hello"
+        text_long = 100*first_name
+        self.element_is_visible(Locators.CONTENT).click()
+        self.element_is_visible(Locators.CREATE_BUTTON).click()
+        self.element_is_visible(Locators.CREATE_ARTICLE).click()
+        time.sleep(5)
+        self.element_is_visible(Locators.NAME_OF_ARTICLE).send_keys(first_name)
+        self.element_is_visible(Locators.FOLDER_SAVE_ARTICLE).send_keys("Контент 1")
+        self.element_is_visible(Locators.TEXT_AREA_ARTICLE).send_keys(text)
+        time.sleep(1)
+        self.element_is_visible(Locators.TEXT_AREA_ARTICLE).send_keys(Keys.LEFT_CONTROL+'a')
+        self.element_is_visible(Locators.TEXT_BOLD_FORMAT).click()
+        self.element_is_visible(Locators.TEXT_ITALIC_FORMAT).click()
+        self.element_is_visible(Locators.TEXT_UNDERLINE_FORMAT).click()
+        """ !!!! ФОРМАТИРОВАНИЕ И ДОБАВЛЕНИЕ ФАЙЛОВ"""
+        # time.sleep(1)
+        # text_color = driver.find_element(By.XPATH, "//span[@class='cke_button_icon cke_button__textcolor_icon']")
+        # text_color.click()
+        # text_color.send_keys(Keys.TAB)
+        # text_color.send_keys(Keys.TAB)
+        # text_color.send_keys(Keys.TAB)
+        # text_color.send_keys(Keys.TAB)
+        # text_color.send_keys(Keys.RETURN)
+        """add media"""
+        # avatar = Path(pathlib.Path.cwd(), "media.jpeg")
+        # path = str(avatar)
+        # self.element_is_visible(Locators.UPLOAD_MEDIA).click()
+        # self.driver.execute_script("""document.querySelector("input[type='file']").style.visibility = 'visible';""")
+        # time.sleep(1)
+        # self.element_is_visible(Locators.UPLOAD_MEDIA_INPUT).send_keys(path)
+        # time.sleep(1)
+        # self.element_is_visible(Locators.UPLOAD_MEDIA).click()
+        self.element_is_visible(Locators.TYPOGRAPHY_ARTICLE).click()
+        navigation_text_check = self.element_is_visible(Locators.NAVIGATION)
+        navigation_text_check_value = navigation_text_check.text
+        assert navigation_text_check_value == "навигация"
+        print(navigation_text_check_value)
+        """check text"""
+        search_text_check = self.element_is_visible(Locators.SEARCH)
+        search_text_check_value = search_text_check.text
+        assert search_text_check_value == "поиск"
+        print(search_text_check_value)
+        access_text_check = self.element_is_visible(Locators.ACCESS)
+        access_text_check_value = access_text_check.text
+        assert access_text_check_value == "доступ"
+        print(access_text_check_value)
+        version_text_check = self.element_is_visible(Locators.VERSION)
+        version_text_check_value = version_text_check.text
+        assert version_text_check_value == "версионность"
+        print(version_text_check_value)
+        self.element_is_visible(Locators.SUBMIT_ARTICLE).click()
+        self.element_is_visible(Locators.SEARCH_INPUT_REQUEST).send_keys(text_long)
+        self.element_is_visible(Locators.ADD_SEARCH_BUTTON).click()
+        self.element_is_visible(Locators.FINISH_BUTTON).click()
+        self.element_is_visible(Locators.SUBMIT_ARTICLE).click()
+
+        self.element_is_visible(Locators.SUBMIT_ARTICLE).click()
+        check_text_role = self.element_is_visible(Locators.CHECK_TEXT_ROLE)
+        check_text_role_value = check_text_role.text
+        assert check_text_role_value == "роль"
+        print(check_text_role_value)
+        # self.element_is_visible(Locators.MYTEST).click()
+        check_select_radio = self.element_is_visible(Locators.CHECK_RADIOBUTTON_TYPOGRAPHY_NOW)
+        check_select_radio.is_selected()
+        print("radiobutton_typography_now True")
+        check_select_radio2 = self.element_is_visible(Locators.CHECK_RADIOBUTTON_NO_DELETE)
+        check_select_radio2.is_selected()
+        print("radiobutton_no_delete True")
+        self.element_is_visible(Locators.FINISH_BUTTON).click()
+        check_text_filled = self.element_is_visible(Locators.CHECK_TEXT_FILLED_NEED)
+        check_text_filled_value = check_text_filled.text
+        assert check_text_filled_value == "Должно быть заполнено"
+        print(check_text_filled_value)
+        self.element_is_visible(Locators.TEXT_AREA_ALERT).send_keys(first_name)
+        self.element_is_visible(Locators.FINISH_BUTTON).click()
+        check_new_article = self.element_is_visible(Locators.CHECK_NEW_ARTICLE)
+        check_new_article_value = check_new_article.text
+        assert check_new_article_value == "Информация о контенте"
+        print(check_new_article_value)
+
+    def fixing_article(self, driver):
+        """FIXING_ARTICLE"""
+        person = generated_person()
+        text_test = person.first_name+str(random.randint(99, 999))
+        text_fixing = 5*(person.last_name+str(random.randint(99, 999)))
+        self.element_is_visible(Locators.CONTENT).click()
+        """create test article"""
+        text_area = "Hello"
+        self.element_is_visible(Locators.CREATE_BUTTON).click()
+        self.element_is_visible(Locators.CREATE_ARTICLE).click()
+        time.sleep(5)
+        self.element_is_visible(Locators.NAME_OF_ARTICLE).send_keys(text_test)
+        self.element_is_visible(Locators.FOLDER_SAVE_ARTICLE).send_keys("Контент 1")
+        self.element_is_visible(Locators.TEXT_AREA_ARTICLE).send_keys(text_area)
+        self.element_is_visible(Locators.TYPOGRAPHY_ARTICLE).click()
+        self.element_is_visible(Locators.SUBMIT_ARTICLE).click()
+        self.element_is_visible(Locators.SUBMIT_ARTICLE).click()
+        self.element_is_visible(Locators.SUBMIT_ARTICLE).click()
+        self.element_is_visible(Locators.TEXT_AREA_ALERT).send_keys(text_area)
+        self.element_is_visible(Locators.FINISH_BUTTON).click()
+        self.element_is_visible(Locators.CLOSE_CREATED_ARTICLE).click()
+        self.element_is_visible(Locators.SEARCH_HEAD_PAGE).click()
+        self.element_is_visible(Locators.BUTTON_FIXING_CONTENT).click()
+        self.element_is_visible(Locators.INPUT_REQUEST).send_keys(text_fixing)
+        time.sleep(1)
+        self.element_is_visible(Locators.BUTTON_FIXING_CONTENT1).click()
+        check_add_fixing_content = self.element_is_visible(Locators.CHECK_ADD_FIXING_CONTENT)
+        check_add_fixing_content_value = check_add_fixing_content.text
+        assert check_add_fixing_content_value == "Добавление закрепленного контента"
+        print(check_add_fixing_content_value)
+        self.element_is_visible(Locators.SEARCH_TEST_ARTICLE).send_keys(text_test)
+        time.sleep(1)
+        self.element_is_visible(Locators.TEST_ARTICLE_NAME).click()
+        self.element_is_visible(Locators.BUTTON_SUBMIT).click()
+        # self.element_is_visible(Locators.BUTTON_SUBMIT).click()
+        check_link_of_content = self.element_is_visible(Locators.CHECK_LINK_OF_CONTENT_RADIO)
+        check_link_of_content.is_selected()
+        check_name_content = self.element_is_visible(Locators.CHECK_NAME_CONTENT)
+        check_name_content_value = check_name_content.text
+        assert check_name_content_value == "Контент 1"
+        print(check_name_content_value)
+        driver.implicitly_wait(5)
+        check_name_article = driver.find_element(By.XPATH, f"//section[@class='m-content-fix-wizard__link']/./p[text()='{text_test}']")
+        check_name_article_value = check_name_article.text
+        assert check_name_article_value == text_test
+        print(check_name_article_value)
+        self.element_is_visible(Locators.INCLUDED_CONTENT_RADIO).click()
+        included_content = self.element_is_visible(Locators.INCLUDED_CONTENT)
+        included_content_value = included_content.text
+        assert included_content_value == text_area
+        print(included_content_value)
+        self.element_is_visible(Locators.FIXING).click()
+        check_number_1_of_list = self.element_is_visible(Locators.LIST_OF_ARTICLES)
+        check_number_1_of_list_value = check_number_1_of_list.text
+        assert check_number_1_of_list_value == text_test
+        print(check_number_1_of_list_value)
+        self.element_is_visible(Locators.POPUP_CLOSE_SVG).click()
+        self.element_is_visible(Locators.SEARCH_OF_CONTENTS).send_keys(text_fixing)
+        # time.sleep(1)
+        self.element_is_visible(Locators.FIND_OF_CONTENT).click()
+        check_text_hello = self.element_is_visible(Locators.CHECK_TEXT_HELLO)
+        check_text_hello_value = check_text_hello.text
+        assert check_text_hello_value == "Hello"
+        print(check_text_hello_value)
+        check_article = driver.find_element(By.XPATH, f"//p[normalize-space()='{text_test}']")
+        check_article_value = check_article.text
+        assert check_article_value == text_test
+        print(check_article_value)
+
+
+
+        #
+        # driver.implicitly_wait(5)
+        # check_created_new_folder = driver.find_element(By.XPATH, f"//span[normalize-space()='{text_fixing}']")
+        # self.driver.execute_script("arguments[0].scrollIntoView();", check_created_new_folder)
+        # # check_created_new_folder = driver.find_element(By.XPATH, f"//span[normalize-space()='{text_fixing}']")
+        # check_created_new_folder.click()
+
+
+
+
+
