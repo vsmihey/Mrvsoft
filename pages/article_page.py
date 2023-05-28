ef8eed05482e64b7028555d4fcef6c0fa3d07022
LAPTOP-E367P135\User <vsmihey@gmail.com>
2023-05-28 22:23:17 +0700

    test2_copy_paste

--- pages/article_page.py
+++ pages/article_page.py
@@ -414,20 +414,21 @@ class ArticlePage(BasePage):
         time.sleep(1)
         name_of_content = driver.find_element(By.XPATH, f"//p[text()='{name_content}']")
         name_of_content.click()
-        time.sleep(1)
+
+        time.sleep(20)
+        self.screenshot()
+        self.element_is_visible(Locators.EDIT_ARTICLE).click()
+        # self.element_is_visible(Locators.EDIT_ARTICLE).click()
+
+        time.sleep(3)
         try:
-            self.element_is_visible(Locators.EDIT_ARTICLE).click()
-        except TimeoutException:
             self.screenshot()
-            name_of_content.click()
-            self.element_is_visible(Locators.EDIT_ARTICLE).click()
-        time.sleep(1)
-        try:
             field4 = self.element_is_visible(Locators.TEXT_FIELD_ONE_MORE)
         except TimeoutException:
             self.screenshot()
-            self.element_is_visible(Locators.EDIT_ARTICLE).click()
+            time.sleep(1)
             field4 = self.element_is_visible(Locators.TEXT_FIELD_ONE_MORE)
+
         field5 = self.element_is_visible(Locators.LINK_FIELD_FOR_CLEAR_1)
         field6 = driver.find_element(By.XPATH, f"//pre[text()='{mail}']")
         for_click = self.element_is_visible(Locators.FOR_CLICK)
