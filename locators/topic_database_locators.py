fabebb3f57c06fb57460f706636aa5339e9dc8cf
LAPTOP-E367P135\User <vsmihey@gmail.com>
2023-06-09 18:04:43 +0700

    test2_create_topics

--- locators/topic_database_locators.py
+++ locators/topic_database_locators.py
@@ -10,7 +10,7 @@ class CreateTopicDatabaseLocators:
     TEXT_NOT_QUESTIONS_NOW = (By.XPATH, "//p[text()='В этом проекте пока нет вопросов. Создайте структуру тем для его размещения']")
     BUTTON_ADD_TOPIC = (By.XPATH, "//p[contains(text(),'Создать темы')]")
     """check text open form new topic"""
-    TEXT_NEW_QUESTION_WINDOW = (By.XPATH, "//h3[contains(text(),'Новая тема')]")
+    TEXT_NEW_QUESTION_TOPIC_WINDOW = (By.XPATH, "//h3[contains(text(),'Новая тема')]")
     TEXT_NAME = (By.XPATH, "//div[text()= 'название']")
     TEXT_PLACEHOLDER_INPUT_NAME_TOPIC = (By.XPATH, "//input[@placeholder='Введите название темы']")
     TEXT_PARENT_TOPIC = (By.XPATH, "//div[text()='родительская тема']")
@@ -18,5 +18,50 @@ class CreateTopicDatabaseLocators:
     BUTTON_CREATE_TOPIC = (By.XPATH, "//footer[@class='m-popup__footer lms-edit-theme-popup__footer']//p[@class='m-ui-typography m-ui-typography--bold m-ui-typography--16x16 m-button__text'][contains(text(),'Создать тему')]") # ПРОВЕРИТЬ
     """input name topic and check len"""
     INPUT_NAME_TOPIC = (By.XPATH, "//input[@placeholder='Введите название темы']")
+    """check text structure"""
+    TEXT_RULE_STRUCTURE = (By.XPATH, "//h3[contains(text(),'Управление структурой')]")
+    TEXT_CREATED_TOPIC_NAME = (By.XPATH, "//div[@class='tree-item-content__name']")
+    """function create new topic"""
+    BUTTON_NEW_TOPIC = (By.XPATH, "//p[contains(text(),'Новая тема')]")
+    """del created topics"""
+    LIST_CREATED_TOPICS = (By.CSS_SELECTOR, "li[class='m-tree-item m-tree-item__wrapper']")
+    BUTTON_DELETE_TOPIC = (By.XPATH, "//button[@class='m-button m-button--danger m-button--medium lms-edit-theme-popup__delete']")
+    BUTTON_CONFIRM_DELETE_TOPIC = (By.XPATH, "//button[@class='m-button m-button--danger m-button--medium popup-confirm__action popup-confirm__danger']")
+    SVG_CLOSE_DELETED_WINDOW = (By.XPATH, "//div[@class='m-popup__close']")
+    """question add"""
+    BUTTON_QUESTION_ADD = (By.XPATH, "//p[contains(text(),'Добавить')]")
+    BUTTON_CHANGE_QUESTION = (By.XPATH, "//p[contains(text(),'Изменить тему')]")
+    TEXT_DATABASE_OF_QUESTION = (By.XPATH, "//p[text()='В этом проекте пока нет вопросов. Вы можете это исправить']")
+    TEXT_DATABASE_OF_QUESTION_HEAD = (By.XPATH, "//h3[contains(text(),'База вопросов')]")
+    """new question form"""
+    TEXT_NEW_QUESTION = (By.XPATH, "//h3[contains(text(),'Новый вопрос')]")
+    TEXTAREA_PLACEHOLDER_INPUT_NAME_QUESTION = (By.XPATH, "//textarea[@placeholder='Введите текст вопроса']")
+    DROPDOWN_TIPE_OF_QUESTION = (By.XPATH, "//div[@class='lms-question-editor__field m-ui-select m-ui-input-wrapper-2']//select[@class='m-ui-select__select']")
+    DROPDOWN_TOPIC = (By.XPATH, "//div[@class='lms-question-editor__input m-ui-select m-ui-input-wrapper-2']//select[@class='m-ui-select__select']")
+    ANSWER_AND_CHECKBOX = (By.XPATH, "//textarea[@placeholder='Введите текст ответа']")
+    ANSWER_CHECKBOX = (By.XPATH, "//label[@class='radio-wrapper lms-answer-field__radio radio-wrapper--no-label']")
+    BUTTON_CREATE_QUESTION = (By.XPATH, "//p[contains(text(),'Создать вопрос')]")
+    """check created window question"""
+    SVG_SLIDERS = (By.XPATH, "//div[@class='themes-side-popup-filter__icon-container']")
+    TEXT_MODAL_WINDOW_TOPICS = (By.XPATH, "//h3[contains(text(),'Темы')]")
+    CHANGE_TOPICS_WINDOW = (By.XPATH, "//span[contains(text(),'изменить')]")
+    TEXT_CREATED_NEW_QUESTION = (By.XPATH, "//span[@class='lms-question-bar__text']")
+    SVG_DEL_QUESTION = (By.XPATH, "(//*[local-name()='svg'][@class='lms-question-bar__icon lms-question-bar__icon-trash'])")
+    SVG_DEL_QUESTION_CONFIRM = (By.XPATH, "//p[contains(text(),'Удалить вопрос')]")
+    """check all created topics"""
+    TEXT_FIRST_TOPIC = (By.XPATH, "(//div[@class='tree-item-content__name'])[1]")
+    DROPDOWN_PARENTS_TOPIC = (By.XPATH, "//select[@class='m-ui-select__select']")
+    BUTTON_SAVE_TOPIC = (By.XPATH, "//p[contains(text(),'Сохранить тему')]")
+    BUTTON_DELETE_TOPIC_CONFIRM = (By.XPATH, "//p[contains(text(),'Удалить тему')]")
+    BUTTON_CREATE_TOPIC_CONFIRM = (By.XPATH, "//p[contains(text(),'Создать тему')]")
+    """check same name and last in list"""""
+    NAME_1 = (By.XPATH, "(//div[contains(text(),'THE SAME NAME')])[1]")
+    NAME_2 = (By.XPATH, "(//div[contains(text(),'THE SAME NAME')])[2]")
+    NAME_2_LAST_IN_LIST = (By.XPATH, "(//li[@class='m-tree-item m-tree-item__wrapper'])[23]")
+
+
+
+
+
 
 
--- pages/topic_database_page.py
+++ pages/topic_database_page.py
@@ -1,6 +1,12 @@
+import random
 import time
 
+from selenium.common import TimeoutException
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+
 from conftest import driver
+from generator.generator import generated_person
 from locators.topic_database_locators import CreateTopicDatabaseLocators
 from pages.base_page import BasePage
 from pages.data_login_password import url
@@ -10,6 +16,7 @@ class CreateTopicDatabase(BasePage):
     Locators = CreateTopicDatabaseLocators
 
     def add_topic_database(self, driver):
+        """ADD TOPIC AND CHECK TEXTS, LEN INPUT NAME"""
         self.input_in_my_project(driver)
         self.element_is_visible(self.Locators.LEARNING_BUTTON).click()
         self.element_is_visible(self.Locators.TAB_ALL_COURSES).click()
@@ -17,8 +24,21 @@ class CreateTopicDatabase(BasePage):
         """check text open form"""
         text_database_of_questions = self.element_is_visible(self.Locators.TEXT_DATABASE_OF_QUESTIONS).text
         assert text_database_of_questions == "База вопросов"
-        text_not_questions_now = self.element_is_visible(self.Locators.TEXT_NOT_QUESTIONS_NOW).text
-        assert text_not_questions_now == "В этом проекте пока нет вопросов. Создайте структуру тем для его размещения"
+        try:
+            text_not_questions_now = self.element_is_visible(self.Locators.TEXT_NOT_QUESTIONS_NOW, timeout=2).text
+            assert text_not_questions_now == "В этом проекте пока нет вопросов. Создайте структуру тем для его размещения"
+        except TimeoutException:
+            try:
+                self.element_is_visible(self.Locators.BUTTON_QUESTION_ADD, timeout=2).click()
+            except TimeoutException:
+                self.element_is_visible(self.Locators.SVG_DEL_QUESTION).click()
+                self.element_is_visible(self.Locators.SVG_DEL_QUESTION_CONFIRM).click()
+                self.element_is_visible(self.Locators.BUTTON_QUESTION_ADD).click()
+
+            self.element_is_visible(self.Locators.BUTTON_CHANGE_QUESTION).click()
+            self.delete_created_topics()
+            self.element_is_visible(self.Locators.SVG_CLOSE_DELETED_WINDOW).click()
+            self.element_is_visible(self.Locators.SVG_CLOSE_DELETED_WINDOW).click()
         button_add_topic = self.element_is_visible(self.Locators.BUTTON_ADD_TOPIC).text
         assert button_add_topic == 'Создать темы'
         self.element_is_visible(self.Locators.BUTTON_ADD_TOPIC).click()
@@ -28,6 +48,141 @@ class CreateTopicDatabase(BasePage):
         self.element_is_visible(self.Locators.INPUT_NAME_TOPIC).send_keys(name_content)
         print(len(name_content), len(get_name))
         assert len(get_name) == 64
+        """create topic"""
+        self.element_is_visible(self.Locators.BUTTON_CREATE_TOPIC).click()
+        """check text open window"""
+        text_rule_structure = self.element_is_visible(self.Locators.TEXT_RULE_STRUCTURE).text
+        assert text_rule_structure == "Управление структурой"
+        text_created_topic_name = self.element_is_visible(self.Locators.TEXT_CREATED_TOPIC_NAME).text
+        assert text_created_topic_name == get_name
+        button_new_topic = self.element_is_visible(self.Locators.BUTTON_NEW_TOPIC).text
+        assert button_new_topic == "Новая тема"
+        self.create_new_topic()
+        """close window created topics"""
+        self.element_is_visible(self.Locators.SVG_CLOSE_DELETED_WINDOW).click()
+        self.create_new_question(driver)
+        # self.delete_created_topics()
+
+    def create_new_topic(self):
+        """CREATE NEW TOPICS"""
+        person = generated_person()
+        for i in range(21):
+            self.element_is_visible(self.Locators.BUTTON_NEW_TOPIC).click()
+            self.element_is_visible(self.Locators.INPUT_NAME_TOPIC).send_keys(person.first_name + str(random.randint(99, 999)))
+            self.element_is_visible(self.Locators.BUTTON_CREATE_TOPIC).click()
+
+    def delete_created_topics(self):
+        """DELETE CREATED TOPICS"""
+        list_created_topics = self.elements_are_visible(self.Locators.LIST_CREATED_TOPICS)
+        for n in list_created_topics:
+            n.click()
+            time.sleep(0.5)
+            self.element_is_visible(self.Locators.BUTTON_DELETE_TOPIC).click()
+            self.element_is_visible(self.Locators.BUTTON_CONFIRM_DELETE_TOPIC).click()
+            #
+            # try:
+            #     self.element_is_visible(self.Locators.BUTTON_CONFIRM_DELETE_TOPIC).click()
+            # except TimeoutException:
+            #     self.element_is_visible(self.Locators.BUTTON_DELETE_TOPIC_CONFIRM).click()
+            #     time.sleep(1)
+
+
+    def create_new_question(self, driver):
+        """CREATE AN CHECK NEW QUESTION"""
+        """check open form"""
+        # person = generated_person()
+        text_of_question = "Здесь должен быть текст вопроса"+str(random.randint(99, 999))
+        answer_of_question = "Ответ на вопрпос"+str(random.randint(99, 999))
+        text_database_of_question_head = self.element_is_visible(self.Locators.TEXT_DATABASE_OF_QUESTION_HEAD).text
+        assert text_database_of_question_head == "База вопросов"
+        text_database_of_question = self.element_is_visible(self.Locators.TEXT_DATABASE_OF_QUESTION).text
+        assert text_database_of_question == "В этом проекте пока нет вопросов. Вы можете это исправить"
+        self.element_is_visible(self.Locators.BUTTON_QUESTION_ADD).click()
+        text_new_question = self.element_is_visible(self.Locators.TEXT_NEW_QUESTION).text
+        assert text_new_question == "Новый вопрос"
+        """fill in the placeholder"""
+        self.element_is_visible(self.Locators.TEXTAREA_PLACEHOLDER_INPUT_NAME_QUESTION).send_keys(text_of_question)
+        """dropdown tipe"""
+        self.element_is_visible(self.Locators.DROPDOWN_TIPE_OF_QUESTION).click()
+        self.element_is_visible(self.Locators.DROPDOWN_TIPE_OF_QUESTION).send_keys(Keys.ARROW_DOWN)
+        self.element_is_visible(self.Locators.DROPDOWN_TIPE_OF_QUESTION).send_keys(Keys.RETURN)
+        """dropdown topic"""
+        self.element_is_visible(self.Locators.DROPDOWN_TOPIC).click()
+        self.element_is_visible(self.Locators.DROPDOWN_TOPIC).send_keys(Keys.ARROW_DOWN)
+        self.element_is_visible(self.Locators.DROPDOWN_TOPIC).send_keys(Keys.RETURN)
+        """answer"""
+        self.element_is_visible(self.Locators.ANSWER_AND_CHECKBOX).send_keys(answer_of_question)
+        self.element_is_visible(self.Locators.ANSWER_CHECKBOX).click()
+        self.element_is_visible(self.Locators.BUTTON_CREATE_QUESTION).click()
+        """created question window"""
+        text_database_of_question_head = self.element_is_visible(self.Locators.TEXT_DATABASE_OF_QUESTION_HEAD).text
+        assert text_database_of_question_head == "База вопросов"
+        # time.sleep(5)
+        text_created_new_question = self.element_is_visible(self.Locators.TEXT_CREATED_NEW_QUESTION).text
+        assert text_created_new_question == text_of_question
+        self.element_is_visible(self.Locators.SVG_SLIDERS).click()
+        text_modal_window_topics = self.element_is_visible(self.Locators.TEXT_MODAL_WINDOW_TOPICS).text
+        assert text_modal_window_topics == "Темы"
+        self.element_is_visible(self.Locators.CHANGE_TOPICS_WINDOW).click()
+        text_rule_structure = self.element_is_visible(self.Locators.TEXT_RULE_STRUCTURE).text
+        assert text_rule_structure == "Управление структурой"
+        """check all created topics"""
+        text_first_topic = self.element_is_visible(self.Locators.TEXT_FIRST_TOPIC).text
+        len_text_first_topic = len(text_first_topic)
+        assert len_text_first_topic == 64
+        self.element_is_visible(self.Locators.TEXT_FIRST_TOPIC).click()
+        self.element_is_visible(self.Locators.TEXT_PLACEHOLDER_INPUT_NAME_TOPIC).clear()
+        self.element_is_visible(self.Locators.TEXT_PLACEHOLDER_INPUT_NAME_TOPIC).send_keys("THE SAME NAME")
+        self.element_is_visible(self.Locators.BUTTON_SAVE_TOPIC).click()
+        """the same name create"""
+        self.element_is_visible(self.Locators.BUTTON_NEW_TOPIC).click()
+        self.element_is_visible(self.Locators.TEXT_PLACEHOLDER_INPUT_NAME_TOPIC).send_keys("THE SAME NAME")
+        self.element_is_visible(self.Locators.BUTTON_CREATE_TOPIC_CONFIRM).click()
+        """check count the same name"""
+        data_same_name = []
+        name_1 = self.elements_is_present(self.Locators.NAME_1)
+        name_2 = self.elements_is_present(self.Locators.NAME_2)
+        data_same_name.append(name_1)
+        data_same_name.append(name_2)
+        len_data_same_name = len(data_same_name)
+        assert len_data_same_name == 2
+        """check last topic"""
+        element = self.elements_is_present(self.Locators.NAME_2)
+        self.action_move_to_element(element)
+        self.elements_is_present(self.Locators.NAME_2).click()
+        name_2_value = self.element_is_visible(self.Locators.TEXT_PLACEHOLDER_INPUT_NAME_TOPIC).get_attribute("value")
+        assert name_2_value == "THE SAME NAME"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
