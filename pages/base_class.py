92e8c134aeb5ec55d5624c7a5ae7fd5ffbdc4337
boris <3678vg1@mail.ru>
2023-07-13 17:32:09 +0500

    Класс для создания базовой статьи

--- pages/base_class.py
+++ pages/base_class.py
@@ -28,3 +28,9 @@ class MainPage:
 
     def element_is_visible(self, locator, timeout=10):
         return Wait(self.browser, timeout).until(EC.visibility_of_element_located(locator))
+
+    def elements_is_present(self, locator, timeout=10):
+        return Wait(self.browser, timeout).until(EC.presence_of_element_located(locator))
+
+    def elements_are_visible(self, locator, timeout=10):
+        return Wait(self.browser, timeout).until(EC.visibility_of_all_elements_located(locator))
